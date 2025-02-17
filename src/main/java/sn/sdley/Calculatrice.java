4ec740fa1811fb1fb56b5c01afa155cea89e16d0
Souleymane DIALLO <sdley2020@gmail.com>
2025-02-17 16:55:38 +0000

    Test d'égalité avec assertEquals()

--- src/main/java/sn/sdley/Calculatrice.java
+++ src/main/java/sn/sdley/Calculatrice.java
@@ -2,6 +2,9 @@ package sn.sdley;
 
 public class Calculatrice {
     public int addition(int a, int b) {
+        if (a < 0 || b < 0) {
+            throw new IllegalArgumentException("Les nombres doivent être positifs");
+        }
         return a + b;
     }
 
--- src/test/java/sn/sdley/CalculatriceTest.java
+++ src/test/java/sn/sdley/CalculatriceTest.java
@@ -22,5 +22,30 @@ public class CalculatriceTest {
         assert calculatrice.addition(2, 3) == 5;
     }
 
+    @Test
+    public void additionValeurNegativeParTry(){
+        try {
+            calculatrice.addition(-1, 2);
+        } catch (IllegalArgumentException e) {
+            assert e.getMessage().equals("Les nombres doivent être positifs");
+        }
+    }
+
+    @Test
+    public void additionValeurNegativeParAssertThrows(){
+        IllegalArgumentException e = org.junit.jupiter.api.Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            calculatrice.addition(-1, 2);
+        });
+        assert e.getMessage().equals("Les nombres doivent être positifs");
+    }
+
+    @Test
+    public void additionValeurNegativeParAssertThrows2(){
+        org.junit.jupiter.api.Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            calculatrice.addition(-1, 2);
+        }, "Les nombres doivent être positifs");
+    }
+
+
 
 }
